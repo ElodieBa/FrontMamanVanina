14916394bfc1cf3caee269527fcbd5525cfe0b4b
amelineS <sehan.ameline@gmail.com>
2019-05-07 17:25:20 +0200

    menu, list commande, detail commande

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CommandeListComponent } from './commande-list/commande-list.component';
+import { CommandeDetailComponent } from './commande-detail/commande-detail.component';
+import { RoutModule } from './rout/rout.module';
+import { MenuComponent } from './menu/menu.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommandeListComponent,
+    CommandeDetailComponent,
+    MenuComponent,
+    AccueilComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RoutModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
