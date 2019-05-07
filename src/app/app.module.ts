4d08ce92da700fd0f0f5aca0219e62fe30664866
LudoPilou <guerlesquin.ludovic@gmail.com>
2019-05-07 15:42:29 +0200

    creation des pages listClients, authentification, navigation (seulement créée mais pas complétée)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ClientListComponent} from './client-list/client-list.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { NavigationComponent } from './navigation/navigation.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientListComponent,
+    AuthentificationComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
