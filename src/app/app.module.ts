3f7d443bb7fb51953fbcb868ab398205d37b33ea
Constance <constance.epinard@gmail.com>
2019-05-07 15:39:39 +0200

    creation de liste facture et detail facture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { FactureListComponent } from './facture-list/facture-list.component';
+import { FactureDetailComponent } from './facture-list/facture-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FactureService } from './facture-list/facture.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FactureListComponent,
+    FactureDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [FactureService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
